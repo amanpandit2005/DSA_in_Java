2d60835ac7423692a0145cf8de29c1647800c27f
Aman Mishra <amanmishra15.08.2005@gmail.com>
2026-04-24 01:51:51 +0530

    Delete src directory

--- src/Loops/ASCII_Values_of_Alphabets.java
+++
@@ -1,8 +0,0 @@
-package L4_L5_Loops;
-
-public class ASCII_Values_of_Alphabets {
-    public static void main(String[] args) {
-        for (int i=65;i<=90;i++)
-            System.out.println((char)i+" "+i);
-    }
-}
