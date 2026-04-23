a08cb284b0192b415bd5f77dc96d9ab357d592ad
amanmishra <amanmishra15.08.2005@gmail.com>
2026-04-24 01:59:18 +0530

    Fixing Repo

--- Basics/Hello_Aman.java
+++
@@ -1,9 +0,0 @@
-package L1_L2_Basics;
-
-public class Hello_Aman {
-    public static void main(String[] args) {
-
-        System.out.print("Hello Aman");
-    }
-
-}
--- Basics/Modulus_Operator.java
+++
@@ -1,8 +0,0 @@
-package L1_L2_Basics;
-
-public class Modulus_Operator {
-    public static void main(String[] args) {
-        System.out.println(69%7);
-        System.out.println(69%6);
-    }
-}
